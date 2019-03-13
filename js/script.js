1dbe4e68de21c9508a3a1e063205c061d8f001c4
Kory Telesford <tkory85@gmail.com>
2019-03-13 21:53:29 +0000

    Java script not done yet

--- js/script.js
+++ js/script.js
@@ -1,80 +1,35 @@
 /* global $ */
 
-// BELOW Update the songs array with four of your favorites songs.
-var songs = ["Shape Of You", "High Hopes", "Crazy", "What about us"];
-// BELOW Add 4 More arrays to store images_links, artists, song lengths, and links for each song
-// Make sure they match the same order as your array above
-var images_links = [
-"https://img.sheetmusic.direct/catalogue/product/smd_h_1225024pTKw50Amm2/large.jpg"
-, "https://i.axs.com/2018/05/54798-image_5b06e97b0c1b5.jpg", "https://upload.wikimedia.org/wikipedia/en/thumb/2/24/GnarlsBarkleyCrazyCover.JPG/220px-GnarlsBarkleyCrazyCover.JPG", 
-"https://i.ytimg.com/vi/wKOEJs602uk/maxresdefault.jpg"];
-var artists = ["Ed", "Panic! at the Disco", "Gnarls", "P!nk"];
-var song_lengths = ["3:53", "", "", ""];
-var links = ["", "", "", ""];
-
-function displaySongInfo(){
-    // BELOW Use forEach Loop to display the data from each of your arrays in the correct div
-
-}
-
-function emptySongInfo(){
-    $("#songs").empty();
-    // Use jQuery to empty all of the remaining divs
-    
-
-}
 
 
-function addSongInfo(){
-    // BELOW write the code to add new items to each of the arrays.
-    var SongName = $(".Song Name").val();
-    var ArtistName = $(".Artist Name").val();
-    var SongLength = $(".Song Length").val();
-    var ImageURL = $(".Image URL").val();
-    var SongLinkAddress = $(".Song Link Address").val();
-    
-    
-    // 3
-    songs.push(SongName);
-    artists.push(ArtistName);
-    song_lengths.push(SongLength);
-    images_links.push(ImageURL);
-    links.push(SongLinkAddress);
-    
-    // 4
-    $(".song").empty();
-    $(".artist").empty();
-    $(".length").empty();
-    $(".image").empty();
-    $(".link").empty();
-     
-    // 5
+var songs = [
+    { song_name: "Shape Of You", images_links: "", artists: "Ed" },
+    { song_name: "High Hopes", images_links: "", artists: "Panic! at the Disco" },
+    { song_name: "Crazy", images_links: "", artists: "Gnarls" },
+    { song_name: "What about us", images_links: "", artists: "P!nk" },
+];
+
+
+function displaySongs() {
+    // write code to display guests here
     songs.forEach(function(song) {
-        $(".song").append(song);
+        $(".songs").append(song.song_name);
+        $(".songs").append(song.images_links);
+        $(".songs").append(song.artists);
     });
-    
-    artists.forEach(function(artist) {
-        $(".artist").append(artist);
-    });
-
-    song_lengths.forEach(function(song_length) {
-        $(".length").append(song_length);
-    });
-    
-    images_links.forEach(function(img_link) {
-        $(".image").append(img_link);
-    });
-    
-    links.forEach(function(address) {
-        $(".link").append(address);
-    }); 
 }
 
-$("#add").click(function() {
-    emptySongInfo();
-    addSongInfo();
-    displaySongInfo();
-   
+$('.addButton').click(function() {
+    $(".songs").empty();
+    var newSong = {
+        song_name: $(".name").val(),
+        images_links: $(".image_link").val(),
+        artists: $(".artist").val(),
+        // set the properties for the guest here
+    };
+    
+    songs.push(newSong);
+    displaySongs();
 });
 
-//displaySongInfo();
+displaySongs();
